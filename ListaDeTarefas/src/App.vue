0828cbbd0357aa8d4c5e7780b0800e7a751a1263
charles-silvaa <charlessilva928@gmail.com>
2021-08-17 11:39:40 -0300

    first commit

---
+++ ListaDeTarefas/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app id="inspire">
+   <!--<v-navigation-drawer
+      v-model="drawer"
+      app -->
+  
+
+    <v-navigation-drawer 
+      v-model="drawer"
+      app
+      >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Vue.js
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Lista de tarefas
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({ 
+      drawer: null,
+       items: [
+          { title: 'Tarefas', icon: 'mdi-view-dashboard', to:'/' },
+          { title: 'Sobre', icon: 'mdi-help-box', to:'/sobre' },
+        ],
+    }),
+  }
+</script>
---
+++ dist/precache-manifest.b6211f6ddbdcfe53cba2699c04fdd144.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4dbe572889fcbbd64c",
+    "url": "/css/chunk-17bc2355.0b413d52.css"
+  },
+  {
+    "revision": "ab47e7cf13ab6bd111e2",
+    "url": "/css/chunk-vendors.c34e7005.css"
+  },
+  {
+    "revision": "ef6fb62861fc17cebc608f68d9da4daa",
+    "url": "/img/cd.ef6fb628.jpeg"
+  },
+  {
+    "revision": "f995ea3788b6c724cd5ef554af3d6888",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce1123dbefa20e198cd2",
+    "url": "/js/app.2a5eb161.js"
+  },
+  {
+    "revision": "2c4dbe572889fcbbd64c",
+    "url": "/js/chunk-17bc2355.219c6f20.js"
+  },
+  {
+    "revision": "3d2339bff675a76fd696",
+    "url": "/js/chunk-2d22d746.7469d321.js"
+  },
+  {
+    "revision": "ab47e7cf13ab6bd111e2",
+    "url": "/js/chunk-vendors.6d83c650.js"
+  },
+  {
+    "revision": "6585c85a39c5c41562e6dda7f8a829c1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
